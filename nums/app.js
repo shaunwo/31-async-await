2671de311b4426ac4e3be7e40cc0a795df672e24
shaunwo <shaunwo@gmail.com>
2022-07-23 19:08:10 -0400

    Completed the async and await code refactoring

--- nums/app.js
+++ nums/app.js
@@ -3,35 +3,41 @@ let outputArea = document.getElementById('output');
 
 // Step 1
 let randomNum = 7;
-axios
-  .get(`${numbersURL}/${randomNum}?json`)
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+async function part1() {
+  let res = await axios
+    .get(`${numbersURL}/${randomNum}?json`)
+    .then(({ data }) => console.log(data.text))
+    .catch((err) => console.log(err));
+}
+part1();
 
 // Step 2
-let multiNums = [11, 45, 15];
-axios
-  .get(`${numbersURL}/${multiNums}?json`)
-  .then(
-    (data) =>
-      //console.log(data)
-      (outputArea.innerHTML += `  <p>${data.data[11]}</p>
-        <p>${data.data[45]}</p>
-        <p>${data.data[15]}</p>`)
-  )
-  .catch((err) => console.log(err));
+let multiNums = [14, 52, 9];
+async function part2() {
+  let res = await axios
+    .get(`${numbersURL}/${multiNums}?json`)
+    .then(
+      ({ data }) =>
+        (outputArea.innerHTML += `  <p>${data[14]}</p>
+        <p>${data[52]}</p>
+        <p>${data[9]}</p>`)
+    )
+    .catch((err) => console.log(err));
+}
+part2();
 
 // Step 3
-let fourPromises = [];
-for (let i = 0; i < 4; i++) {
-  fourPromises.push(axios.get(`${numbersURL}/random?json`));
+async function part3() {
+  let ranNums = await Promise.all([
+    axios.get(`${numbersURL}/random?json`),
+    axios.get(`${numbersURL}/random?json`),
+    axios.get(`${numbersURL}/random?json`),
+    axios.get(`${numbersURL}/random?json`),
+  ]);
+
+  outputArea.innerHTML += `  <p>${ranNums[0].data.text}</p>`;
+  outputArea.innerHTML += `  <p>${ranNums[1].data.text}</p>`;
+  outputArea.innerHTML += `  <p>${ranNums[2].data.text}</p>`;
+  outputArea.innerHTML += `  <p>${ranNums[3].data.text}</p>`;
 }
-Promise.all(fourPromises)
-  .then((numsArr) =>
-    numsArr.forEach(
-      (data) =>
-        //console.log(data.data.text)
-        (outputArea.innerHTML += `  <p>${data.data.text}</p>`)
-    )
-  )
-  .catch((err) => console.log(err));
+part3();
